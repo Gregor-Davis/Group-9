dd87f6eae8dba7cf4f61a6262adebed77a3a4387
mraza00 <43144437+mraza00@users.noreply.github.com>
2019-12-06 19:01:44 +0000

    redid some java and CSS

--- js/home.js
+++ js/home.js
@@ -1,3 +1,5 @@
+//Get the button:
+
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -5,9 +7,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    myBtn.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    myBtn.style.display = "none";
   }
 }
 
@@ -15,21 +17,4 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
 }
