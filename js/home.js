9b4546cbe31148a435cc714f571aa822c3651729
mraza00 <43144437+mraza00@users.noreply.github.com>
2019-12-06 19:26:37 +0000

    housekeeping

--- js/home.js
+++ js/home.js
@@ -1,8 +1,8 @@
-//Get the button:
+
 
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -13,10 +13,10 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 ////////////////////////////
