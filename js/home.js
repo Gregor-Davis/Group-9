6a774b53fa07c191d355144bfd94513fda5c1d85
mraza00 <43144437+mraza00@users.noreply.github.com>
2019-12-06 19:04:26 +0000

    some minor adjustments

--- js/home.js
+++ js/home.js
@@ -6,7 +6,7 @@ mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
     myBtn.style.display = "block";
   } else {
     myBtn.style.display = "none";
